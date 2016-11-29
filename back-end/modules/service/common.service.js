b62f3a46069f8dd1c398331316b3e4970d0831dc
yuban12315 <fengyulin@imudges.com>
2016-11-29 21:58:38 +0800

    build

--- back-end/modules/service/common.service.js
+++ back-end/modules/service/common.service.js
@@ -2,6 +2,7 @@ var crypto=require('crypto');
 
 var common=function () {
   this.name="common functions";
+  /*Hmac加密*/
   this.Hmac=function (value,key) {
     return crypto.createHmac('sha1',key).update(value).digest('hex');
   };
